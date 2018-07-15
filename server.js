27709b19ea1357ba0bb788884940bd84022e2285
amit shakya <amit0shakya@gmail.com>
2018-07-14 23:08:51 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+	res.send('hello world')
+})
+
+app.listen(port,function(){
+	console.log("app running on port "+port)
+})
